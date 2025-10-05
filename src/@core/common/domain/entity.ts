978663c223b47de66d7f92cddfb911fc04a485d3
Cesar Schutz <cesar.fagundes@e-unicred.com.br>
2025-10-05 15:03:50 -0300

    Adiciona método equals à classe Entity para comparação de instâncias, utilizando lodash para verificar igualdade profunda.

--- src/@core/common/domain/entity.ts
+++ src/@core/common/domain/entity.ts
@@ -1,3 +1,23 @@
+import isEqual from 'lodash/isEqual';
+
 export abstract class Entity {
+    readonly id: any;
+
     abstract toJSON(): any;
+
+    public equals(obj: this): boolean {
+        if (obj === null || obj === undefined) {
+          return false;
+        }
+    
+        if (obj === undefined) {
+          return false;
+        }
+    
+        if (obj.constructor.name !== this.constructor.name) {
+          return false;
+        }
+    
+        return isEqual(this, obj);
+      }
 }
