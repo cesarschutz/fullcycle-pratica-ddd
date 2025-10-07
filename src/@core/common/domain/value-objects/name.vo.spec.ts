56ff4270bfa82dea2ef60a0950e207a5925631c3
Cesar Schutz <cesar.fagundes@e-unicred.com.br>
2025-10-07 19:03:34 -0300

    Adiciona configuração do ESLint, novos arquivos de configuração e módulos para gerenciamento de eventos, além de implementar repositórios e serviços para entidades como Customer, Event e Partner. Remove arquivos e classes obsoletos, simplificando a estrutura do projeto.

--- src/@core/common/domain/value-objects/name.vo.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Name } from './name.vo';
-
-test('deve criar um nome válido', () => {
-  const name = new Name('aaaaaa');
-  expect(name.value).toBe('aaaaaa');
-  console.log(name.toString());
-});
--- src/@core/events/domain/repositories/customer-repository.interface.ts
+++
@@ -1,5 +0,0 @@
-import { IRepository } from '../../../common/domain/repository-interface';
-import { Customer } from '../entities/customer.entity';
-
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface ICustomerRepository extends IRepository<Customer> {}
